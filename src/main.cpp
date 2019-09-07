9da449cd96ba325f8c12c3588196e6ee356902a4
Junho Ryu <jryu@users.noreply.github.com>
2019-09-06 22:17:59 -0700

    Add config.proto and repeat the pattern.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 
 #include <cairo.h>
 #include <cairo-pdf.h>
+#include <fcntl.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/text_format.h>
+#include <stdio.h>
+
+#include "config.pb.h"
+
+config::MacaronTemplatesConfig conf;
+
+double radius() {
+	return (conf.diameter() + conf.buffer()) / 2;
+}
 
 void draw_pattern(cairo_t *cr, double x, double y)
 {
@@ -14,34 +26,78 @@ void draw_pattern(cairo_t *cr, double x, double y)
 	cairo_line_to(cr, x, y + marker_size);
 	cairo_stroke(cr);
 
-	// 1.5 inch circle
-	cairo_arc(cr, x, y, 75, 0, 2*M_PI);
+	// circle
+	cairo_arc(cr, x, y, conf.diameter() / 2, 0, 2*M_PI);
 	cairo_stroke(cr);
 
 	double dashes[] = {5, 5};
 	cairo_set_dash(cr, dashes, sizeof(dashes) / sizeof(dashes[0]), 0);
 	cairo_set_line_width(cr, 1.5);
 
-	// 1.25 inch circle
-	cairo_arc(cr, x, y, 62.5, 0, 2*M_PI);
+	// Smaller buffer
+	cairo_arc(cr, x, y, (conf.diameter() - conf.buffer()) / 2, 0, 2*M_PI);
 	cairo_stroke(cr);
 
-	// 1.75 inch circle
-	cairo_arc(cr, x, y, 87.5, 0, 2*M_PI);
+	// Larger buffer 
+	cairo_arc(cr, x, y, (conf.diameter() + conf.buffer()) / 2, 0, 2*M_PI);
 	cairo_stroke(cr);
 
 	cairo_set_dash(cr, nullptr, 0, 0);
 }
 
+void draw_row(cairo_t *cr, double y, bool indent)
+{
+	double x = radius() + conf.margin();
+	if (indent) {
+		x += (conf.diameter() + conf.buffer() + conf.padding()) / 2;
+	}
+	while (x + radius() + conf.margin() < conf.canvas_width()) {
+		draw_pattern(cr, x, y);
+		x += conf.diameter() + conf.buffer() + conf.padding();
+	}
+}
+
+bool parse_config()
+{
+	// Verify that the version of the library that we linked
+	// against is compatible with the version of the headers we
+	// compiled against.
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+	int fd = open("config.txt", O_RDONLY);
+	if (fd < 0) {
+		return false;
+	}
+	google::protobuf::io::FileInputStream fileInput(fd);
+	fileInput.SetCloseOnDelete(true);
+
+	if (!google::protobuf::TextFormat::Parse(&fileInput, &conf)) {
+		// protobuf prints error message
+		return false;
+	}
+	return true;
+}
+
 int main()
 {
+	if (!parse_config()) {
+		return EXIT_FAILURE;
+	}
+
 	cairo_surface_t *surface = cairo_pdf_surface_create("example.pdf",
-			200, 200);
+			conf.canvas_width(), conf.canvas_height());
 	cairo_t *cr = cairo_create(surface);
 
-	draw_pattern(cr, 100, 100);
+	bool indent = false;
+	for (int y = radius() + conf.margin();
+			y + radius() + conf.margin() < conf.canvas_height();
+			y += (conf.diameter() + conf.buffer() + conf.padding()) *
+			sin(M_PI / 3)) {
+		draw_row(cr, y, indent);
+		indent = !indent;
+	}
 
 	cairo_destroy(cr);
 	cairo_surface_destroy(surface);
-	return 0;
+	return EXIT_SUCCESS;
 }
